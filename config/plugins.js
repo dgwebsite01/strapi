8b06bed93437f5f1583f1f0b3ffdd35389352187
DimitrisDavidGerokonstantis <dimitrisger2@gmail.com>
2025-02-24 18:48:29 +0200

    Add email provider

--- config/plugins.js
+++ config/plugins.js
@@ -11,4 +11,21 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: "smtp.gmail.com",
+        port: 465,
+        auth: {
+          user: env("GMAIL_USER"),
+          pass: env("GMAIL_PASS"),
+        },
+      },
+      settings: {
+        defaultFrom: env("GMAIL_USER"),
+        defaultReplyTo: env("GMAIL_USER"),
+      },
+    },
+  },
 });
