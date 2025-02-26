37c399a6175318d85d663b485e51dc568af165c8
DimitrisDavidGerokonstantis <dimitrisger2@gmail.com>
2025-02-26 12:40:26 +0200

    Restrict access

---
+++ src/middlewares/restrict-origin.js
@@ -0,0 +1,23 @@
+module.exports = (config, { strapi }) => {
+  return async (ctx, next) => {
+    const allowedOrigins = [process.env.FRONTEND_URL, process.env.STRAPI_URL];
+    const requestOrigin = ctx.request.headers.origin;
+    const allowedMethodsFromFrontend = ["GET", "POST"];
+
+    if (
+      allowedOrigins.length === 0 ||
+      !allowedOrigins.includes(requestOrigin)
+    ) {
+      ctx.throw(403, "Forbidden: Access is not allowed from this origin");
+    }
+
+    if (
+      requestOrigin === process.env.FRONTEND_URL &&
+      !allowedMethodsFromFrontend.includes(ctx.method.toUpperCase())
+    ) {
+      ctx.throw(405, `Method Not Allowed: ${ctx.method} is not permitted`);
+    }
+
+    await next();
+  };
+};
